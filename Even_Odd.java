875999df5f193ccbc49fe7aa031ae0351c507602
Raju <rajubhojane2004@gmail.com>
2024-07-08 17:04:51 +0530

    Two Pragrams Done In this Program

---
+++ Even_Odd.java
@@ -0,0 +1,29 @@
+class Even_Odd
+{
+    public static void main(String args[])
+    {
+        int a=5;
+        String val=(a%2==0)?"even":"odd";
+        System.out.println(val);
+
+        int b,c;
+        b=10;c=5;
+        String num=(a>=b)?"A is Greater":"B Is Greater";
+        System.out.println(num);
+
+
+    }
+}
+
+// class Even_Odd
+// {
+//     public static void main(String args[])
+//     {
+//         int a = 5;
+//         String val = (a % 2 == 0) ? "even" : "odd";
+//         System.out.println(val);
+//     }
+// }
+
+
+
