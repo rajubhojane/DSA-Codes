5392b8aad36c24ea654d4987519e0f9ef68f2cd9
Raju <rajubhojane2004@gmail.com>
2024-07-11 13:46:00 +0530

    Sum of 1 to 5

---
+++ forloop2.java
@@ -0,0 +1,12 @@
+public class forloop2 {
+    public static void main(String args[])
+    {
+        int sum=0;
+        for(int i=1;i<=5;i++)
+        {
+          sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+    
+}
