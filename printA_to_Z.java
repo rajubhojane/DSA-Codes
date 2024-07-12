bdea08b349b29f29fed4e1adff54fa3f8e578c4e
Raju <rajubhojane2004@gmail.com>
2024-07-12 08:53:48 +0530

    printA_to_Z.java

---
+++ printA_to_Z.java
@@ -0,0 +1,12 @@
+public class printA_to_Z {
+    public static void main(String args[])
+    {
+        char ch='A';
+        for(int i=1;i<=26;i++)
+        {
+            System.out.println(ch);
+            ch++;
+        }
+    }
+    
+}
